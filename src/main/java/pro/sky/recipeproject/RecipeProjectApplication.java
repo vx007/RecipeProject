e9d5c407fc857ed5eb471736bba85baf258cbe85
vx007 <vasilyev.x@mail.ru>
2022-12-09 20:35:27 +0300

    Initial commit

---
+++ src/main/java/pro/sky/recipeproject/RecipeProjectApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.recipeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/recipeproject/RecipeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.recipeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
