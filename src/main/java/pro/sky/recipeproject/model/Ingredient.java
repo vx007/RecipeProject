2bd134c756213c250b2cc0b60c16750c2e10c8e6
vx007 <vasilyev.x@mail.ru>
2023-01-04 19:56:36 +0300

    Homework #5

--- src/main/java/pro/sky/recipeproject/model/Ingredient.java
+++ src/main/java/pro/sky/recipeproject/model/Ingredient.java
@@ -2,9 +2,11 @@ package pro.sky.recipeproject.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Ingredient {
     private String name;
     private int count;
--- src/main/java/pro/sky/recipeproject/service/IngredientService.java
+++ src/main/java/pro/sky/recipeproject/service/IngredientService.java
@@ -1,48 +1,95 @@
 package pro.sky.recipeproject.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import pro.sky.recipeproject.model.Ingredient;
 
+import javax.annotation.PostConstruct;
+import java.nio.file.Path;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 @Service
 public class IngredientService {
-    private static final Map<Integer, Ingredient> ingredients = new LinkedHashMap<>();
+
+    private final FileService fileService;
+
+    private static LinkedHashMap<Integer, Ingredient> ingredients = new LinkedHashMap<>();
+
     private static int id;
 
-    public int addIngredient(Ingredient ingredient){
+    @Value("${dataFilePath}")
+    private String dataFilePath;
+
+    @Value("${ingredientsFileName}")
+    private String dataFileName;
+
+    public IngredientService(FileService fileService) {
+        this.fileService = fileService;
+    }
+
+    @PostConstruct
+    private void init() {
+        readFromFile();
+    }
+
+    public int addIngredient(Ingredient ingredient) {
         ingredients.put(++id, ingredient);
+        saveToFile();
         return id;
     }
 
-    public boolean editIngredient(int id, Ingredient ingredient){
-        if(ingredients.containsKey(id)){
+    public boolean editIngredient(int id, Ingredient ingredient) {
+        if (ingredients.containsKey(id)) {
             ingredients.put(id, ingredient);
+            saveToFile();
             return true;
         } else {
             return false;
         }
     }
 
-    public boolean deleteIngredient(int id){
-        if(ingredients.containsKey(id)){
+    public boolean deleteIngredient(int id) {
+        if (ingredients.containsKey(id)) {
             ingredients.remove(id);
+            saveToFile();
             return true;
         } else {
             return false;
         }
     }
 
-    public Ingredient getIngredient(int id){
-        if (ingredients.containsKey(id)){
+    public Ingredient getIngredient(int id) {
+        if (ingredients.containsKey(id)) {
             return ingredients.get(id);
-        }else {
+        } else {
             return null;
         }
     }
 
-    public Map<Integer, Ingredient> getAllIngredients(){
+    public Map<Integer, Ingredient> getAllIngredients() {
         return ingredients;
+    }
+
+    private void saveToFile() {
+        try {
+            String json = new ObjectMapper().writeValueAsString(ingredients);
+            fileService.save(Path.of(dataFilePath, dataFileName), json);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void readFromFile() {
+        try {
+            String json = fileService.read(Path.of(dataFilePath, dataFileName));
+            ingredients = new ObjectMapper().readValue(json, new TypeReference<LinkedHashMap<Integer, Ingredient>>() {
+            });
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
